7e6ac011e33194ab9041d3e2fa96a46d2bbbe6af
bendn <bend.n@outlook.com>
2023-06-23 19:17:53 +0700

    update

---
+++ src/base/StringHandler.java
@@ -0,0 +1,8 @@
+package base;
+
+public class StringHandler {
+    public static final String[] badNames = { "shit", "fuck", "asshole", "cunt", "nigger", "nigga", "niga", "faggot",
+            "dick", "bitch", "admin", "mod", "mindustry", "server", "owner", "<", ">", "\\]", "\\[", "nazi" };
+
+    public static final String[] badWords = { "cunt", "nigger", "nigga", "niga", "faggot" };
+}
